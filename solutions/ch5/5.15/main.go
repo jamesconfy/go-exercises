6f71084b682031e0f8d5cae75b4f3222cbcde065
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2023-01-16 12:26:07 +0100

    Added solutions

---
+++ solutions/ch5/5.15/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	mySlice := []int{1, 2, 3, 4, 5, 6, 8, 9, 11, 200, -50}
+	fmt.Printf("Max value is: %d\n", max(mySlice...))
+	fmt.Printf("Min value is: %d\n", min(mySlice...))
+	fmt.Println("Working")
+}
+
+func max(vals ...int) int {
+	if len(vals) == 0 {
+		return 0
+	}
+
+	maxVal := int(math.Inf(-1))
+	for _, val := range vals {
+		if val > maxVal {
+			maxVal = val
+		}
+	}
+	return maxVal
+}
+
+func min(vals ...int) int {
+	if len(vals) == 0 {
+		return 0
+	}
+
+	minVal := 1000000000000000000
+	// fmt.Println(minVal)
+	for _, val := range vals {
+		if val < minVal {
+			minVal = val
+		}
+		// fmt.Println(minVal)
+	}
+	return minVal
+}
---
+++ solutions/ch5/5.16/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var list = []string{"James", "Confidence", "Is", "A", "Boy"}
+	fmt.Printf("Joined list is: %s\n", Join(list...))
+}
+
+func Join(vals ...string) string {
+	var result string
+
+	for _, val := range vals {
+		result += val
+		result += "\t"
+	}
+
+	return result
+}
